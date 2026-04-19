a3cb7b76e539f86ecb6ca56b4dc48523c3440310
Run Qi (Jack) Li <li.jack0707@gmail.com>
2026-04-18 20:52:49 -0400

    chore(web): consolidate VehicleClassData export into single line

--- packages/web/src/modules/classes/index.ts
+++ packages/web/src/modules/classes/index.ts
@@ -1,5 +1,4 @@
-export type { VehicleClassData } from './api'
-export { fetchClassBySlug, fetchClasses } from './api'
+export { fetchClassBySlug, fetchClasses, type VehicleClassData } from './api'
 export { ClassCatalogCard } from './components/ClassCatalogCard'
 export { ClassDetailView } from './components/ClassDetailView'
 export { ClassList } from './components/ClassList'
